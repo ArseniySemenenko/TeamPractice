988bc480d72a376ff069720bbf8c0d3a6714b421
ArseniySemenenko <arseniysemenenko65@gmail.com>
2026-07-07 13:20:14 +0300

    feat(auth-page.html, auth-page.ts): create login page base structure

--- src/app/pages/auth-page/auth-page.ts
+++ src/app/pages/auth-page/auth-page.ts
@@ -1,9 +1,12 @@
 import { Component, input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
+import {MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-auth-page',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatButtonModule , MatFormFieldModule , MatInputModule],
   templateUrl: './auth-page.html',
   styleUrl: './auth-page.css',
 })
