28db229bef58bb9c820062db39825fb881a0a90b
ArseniySemenenko <arseniysemenenko65@gmail.com>
2026-07-08 15:55:27 +0300

    style: fix styles

--- src/app/pages/auth-page/auth-page.ts
+++ src/app/pages/auth-page/auth-page.ts
@@ -1,15 +1,17 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
-import {MatButtonModule } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-auth-page',
-  imports: [RouterLink, RouterLinkActive, MatButtonModule , MatFormFieldModule , MatInputModule],
+  imports: [RouterLink, RouterLinkActive, MatButtonModule , MatFormFieldModule , MatIconModule , MatInputModule],
   templateUrl: './auth-page.html',
   styleUrl: './auth-page.css',
 })
 export class AuthPage {
+  hidePassword = signal<boolean>(true);
   filter = input<string>("login");
 }
